91f7a286c46d120455a8ba3b5c62189b41632459
Fidel de J. Miranda Gallego <69002384+fjmg84@users.noreply.github.com>
2024-01-23 08:51:13 -0500

    fix styles mobil viewport

--- src/components/scrapping.svelte
+++ src/components/scrapping.svelte
@@ -1,9 +1,9 @@
 <div>
 	<form
 		method="post"
-		class="bg-white flex justify-between rounded-full overflow-hidden max-w-[500px]"
+		class="bg-white flex flex-col md:flex-row justify-between rounded-full md:overflow-hidden max-w-[500px]"
 	>
-		<input type="url" name="url" class="text-zinc-900 p-2 pl-5 w-full rounded-l-full" />
+		<input type="url" name="url" class="text-zinc-900 p-2 pl-5 w-full md:rounded-l-full" />
 		<button type="submit" class="bg-slate-200 text-zinc-900 px-5 py-2 text-nowrap"
 			>Add site to bookmark</button
 		>
